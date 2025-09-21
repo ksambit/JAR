ea483297b66bda6f93add44ad144a074f6ca73ab
Sambit Mohanty <ksambit812@gmail.com>
2025-09-21 21:36:57 +0530

    few changes to the main.py file

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import eel
 from engine.features import *
 from engine.command import *
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def list_files(dir='.'):
+    try:
+        return os.listdir(dir)
+    except OSError:
+        return []
+
 eel.init("front")
+
 playAssistantSound()
 
 eel.start('index.html', mode='chrome', host='localhost', block=True)
